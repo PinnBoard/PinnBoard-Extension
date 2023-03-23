38ae8eaf306af389b18ef84ac94a201c776f09e1
subhradwi01 <subhradwi01@gmail.com>
2023-03-23 23:11:58 +0530

    Bookmark script for youtube added

--- contentScript.js
+++ contentScript.js
@@ -1,37 +1,35 @@
-
 // const btn=document.getElementById("segmented-like-button")
-// Message Listers
-(()=>{
-  
-  const newVideLoaded = (v) =>{
-    const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-    console.log(bookmarkBtnExists)
-    if (!bookmarkBtnExists) {
-      const bookmarkBtn = document.createElement("img");
 
-      bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
-      bookmarkBtn.className = "ytp-button " + "bookmark-btn";
-      bookmarkBtn.title = "Click to bookmark current timestamp";
+const newVideLoaded = () => {
+  const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+  console.log(bookmarkBtnExists);
+  if (!bookmarkBtnExists) {
+    const bookmarkBtn = document.createElement("img");
 
-      let youtubeBtnControls = document.getElementsByClassName("ytp-left-controls")[0];
+    bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+    bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+    bookmarkBtn.title = "Click to bookmark current timestamp";
+
+    let youtubeBtnControls =
+    document.querySelector("#owner.item.style-scope.ytd-watch-metadata");
+    console.log(youtubeBtnControls)
     
-      youtubeBtnControls.appendChild(bookmarkBtn);
-      bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
-    }
-    // bookmarkBtn.addEventListener("click",()=> addEventListener(videoLink));
+    bookmarkBtn.style.width="30px"
+    bookmarkBtn.style.paddingLeft="20px"
+
+    youtubeBtnControls.appendChild(bookmarkBtn);
+    bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
   }
-  
-  
-  chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    const { type,videoId, videoLink } = obj;
-    console.log(type)
-    if (type === "NEW_YOUTUBE") {
-      newVideLoaded()
-    }
-  });
+  // bookmarkBtn.addEventListener("click",()=> addEventListener(videoLink));
+};
 
-  newVideLoaded()
-
-})
-
-
+// Message Listers
+chrome.runtime.onMessage.addListener((obj, sender, response) => {
+  const { type, videoId, videoLink } = obj;
+  console.log(type);
+  if (type === "NEW_YOUTUBE") {
+    console.log(videoLink);
+    newVideLoaded()
+  }
+  response({message:"Connection Established"})
+});
