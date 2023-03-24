317c734390a7665a53e5288208dd857b0f6820fa
subhradwi01 <subhradwi01@gmail.com>
2023-03-24 23:46:17 +0530

    Youtube channel bookmark added

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 chrome.tabs.onUpdated.addListener((tabId, _, tab) => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     let activeTab = tabs[0];
+    console.log(activeTab);
     // 1. Youtube funtions
     if (activeTab.url && activeTab.url.includes("youtube.com/watch")) {
       const queryParameters = activeTab.url.split("?")[1];
@@ -18,7 +19,25 @@ chrome.tabs.onUpdated.addListener((tabId, _, tab) => {
         }
       );
     }
-  });
+    // For profile visit
+    else{
+      chrome.tabs.sendMessage(
+        activeTab.id,
+        {
+          type: "NEW_YOUTUBE_PROFILE",
+          videoId: "@"+activeTab.url.split("/")[0],
+          videoLink: activeTab.url,
+        },
+        (res) => {
+          console.log(res);
+        }
+      );
+    }
+  }
+  //For profile
+  
+  
+  );
 
   // 2. LinkedIn functions
 
--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,12 @@
 // const btn=document.getElementById("segmented-like-button")
 
-const newVideLoaded = () => {
-  const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+const newYoutubeProfileLoaded = () => {
+  const youtubeBtnControls = document.querySelector(
+    "#buttons.style-scope.ytd-c4-tabbed-header-renderer"
+  );
+  const bookmarkBtnExists =
+    youtubeBtnControls.getElementsByClassName("bookmark-btn")[0];
+
   console.log(bookmarkBtnExists);
   if (!bookmarkBtnExists) {
     const bookmarkBtn = document.createElement("img");
@@ -10,12 +15,12 @@ const newVideLoaded = () => {
     bookmarkBtn.className = "ytp-button " + "bookmark-btn";
     bookmarkBtn.title = "Click to bookmark current timestamp";
 
-    let youtubeBtnControls =
-    document.querySelector("#owner.item.style-scope.ytd-watch-metadata");
-    console.log(youtubeBtnControls)
-    
-    bookmarkBtn.style.width="30px"
-    bookmarkBtn.style.paddingLeft="20px"
+    // let youtubeBtnControls =
+    // document.querySelector("#buttons.style-scope.ytd-c4-tabbed-header-renderer");
+    console.log(youtubeBtnControls);
+
+    bookmarkBtn.style.width = "30px";
+    bookmarkBtn.style.paddingLeft = "20px";
 
     youtubeBtnControls.appendChild(bookmarkBtn);
     bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
@@ -23,13 +28,43 @@ const newVideLoaded = () => {
   // bookmarkBtn.addEventListener("click",()=> addEventListener(videoLink));
 };
 
+const newVideLoaded = () => {
+  const youtubeBtnControls = document.querySelector(
+    "#owner.item.style-scope.ytd-watch-metadata"
+  );
+  const bookmarkBtnExists =
+    youtubeBtnControls.getElementsByClassName("bookmark-btn")[0];
+  console.log(bookmarkBtnExists);
+  if (!bookmarkBtnExists) {
+    const bookmarkBtn = document.createElement("img");
+
+    bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+    bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+    bookmarkBtn.title = "Click to bookmark current timestamp";
+
+    console.log(youtubeBtnControls);
+
+    bookmarkBtn.style.width = "30px";
+    bookmarkBtn.style.paddingLeft = "20px";
+
+    youtubeBtnControls.appendChild(bookmarkBtn);
+    bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+  }
+  // bookmarkBtn.addEventListener("click",()=> addEventListener(videoLink));
+};
+
+
+
 // Message Listers
 chrome.runtime.onMessage.addListener((obj, sender, response) => {
   const { type, videoId, videoLink } = obj;
   console.log(type);
   if (type === "NEW_YOUTUBE") {
     console.log(videoLink);
-    newVideLoaded()
+    newVideLoaded();
   }
-  response({message:"Connection Established"})
+  if (type === "NEW_YOUTUBE_PROFILE") {
+    newYoutubeProfileLoaded();
+  }
+  response({ message: "Connection Established" });
 });
